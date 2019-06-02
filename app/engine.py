0d0d18d7c97b98108591fe368595f763f231f6c0
RERSY <racillesantos_ratunil@yahoo.com>
2019-06-02 20:25:07 +0800

    engine modified

--- app/engine.py
+++ app/engine.py
@@ -10,22 +10,16 @@ student = Student.query.filter_by(studid=current_user.studid).first()
 #query -> student info
 studinfo = SemesterStudent.query.filter_by(studid=current_user.studid).first()
 #query -> previous courses and grades
-prevcourse_grade = Registration.query.filter_by(subjcode=current_user.subjcode,grade=grade)
+prevcourse_grade = Registration.query.filter_by(subjcode=current_user.subjcode,grade=current_user.grade).all()
 maxsem = 12
 #query -> residency = count all semesters of student
-#remaining semester
-rem_sem = maxsem - residency
-subj = Subject.query.filter_by(subjcode)
-prog = Program.query.filter_by(progcode)
-curri = Curriculum.query.filter_by(progcode)
-dept  = Department.query.filter_by(deptcode)
+subj = Subject.query.filter_by(subjcode).all()
+prog = Program.query.filter_by(progcode).all()
 
 num_deg = Program.query.filter_by(progcode).count()
 all_deg = range(num_deg)
-
-
-#query -> program, curri, subj, dept from db
-#query -> num degree
+num_sub = Subject.query.filter_by(subjcode).count()
+all_sub = range(num_sub)
 
 
 #model
@@ -41,10 +35,28 @@ sbj = model.#allsubjects
 #genconstraints
 
 #academic status #note: if regular 18+, warning 18-, probation 12- units
+ac_st = [1,2,3]
+ac_st[1]='Regular'
+ac_st[2]='Warning'
+ac_st[3]='Probation'
 
 
-model.Add(grade>=3)
-model.Add()
+if n in ac_st[n] ==1:
+    if residency >= 8 and studlevel == 4:
+        model.Add(sem_units >= 3)
+    else:
+        model.Add(sem_units>=18)
+if n in ac_st[n]==2:
+    model.Add(sem_units<18)
+if n in ac_st[n]==3:
+    model.Add(sem_units<=12)
+
+#gpa 
+model.Add(grade<=3)
+model.Add(residency <= maxsem)
+model.Add(gpa <= 3)
+#constraint, no more than 4 failure grade on same sem.
+
 #deptconstraints
 #CS
 model.Add(grade(degree(Comsci) <= 2.5))
@@ -63,7 +75,8 @@ model.Add(prevcourse = Math 60)
 model.Add(sem = 1)
 #edPysEdMat
 model.Add(student gpa <= 2.0)
-model.Add(if studentyear>1, prevcourse(degree(Ed)))
+model.Add(if residency >2: prevcourse(degree(Ed))
+
 
 
 
